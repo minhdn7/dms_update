3e3929889eace6b7e251aa82ddc27fca9b444472
minhdn7 <minhdn231@gmail.com>
2018-07-13 17:29:35 +0700

    update code 13/7/2018

--- app/src/main/java/vn/com/vsc/ptpm/VNPT_DMS/model/response/tmx/danhsachtuyen/DanhSachVungResponse.java
+++ app/src/main/java/vn/com/vsc/ptpm/VNPT_DMS/model/response/tmx/danhsachtuyen/DanhSachVungResponse.java
@@ -36,4 +36,9 @@ public class DanhSachVungResponse {
         this.id = id;
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
---
+++ app/src/main/java/vn/com/vsc/ptpm/VNPT_DMS/presenter/tmx/danhsachtuyen/ILayDanhSachPhuongTMXPresenter.java
@@ -0,0 +1,5 @@
+package vn.com.vsc.ptpm.VNPT_DMS.presenter.tmx.danhsachtuyen;
+
+public interface ILayDanhSachPhuongTMXPresenter {
+    void layDanhSachPhuong(String districtId);
+}
